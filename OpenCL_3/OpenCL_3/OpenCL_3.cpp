55a9bafa5dc6217586d3369324a9020eb9c58c16
Jaeqx,Lars L.J.J <l.jaeqx@student.fontys.nl>
2017-03-07 21:07:34 +0100

    Assignment 3 done

--- OpenCL_3/OpenCL_3/OpenCL_3.cpp
+++ OpenCL_3/OpenCL_3/OpenCL_3.cpp
@@ -30,6 +30,8 @@ int main() {
 	
 	cl_mem dev_g_data = NULL;
 
+	LARGE_INTEGER timeWriteFreq, timeWriteStart, timeWriteEnd, timeCalcFreq, timeCalcStart, timeCalcEnd, timeReadFreq, timeReadStart, timeReadEnd;
+
 	size_t infoSize;
 	char* info;
 	
@@ -37,13 +39,24 @@ int main() {
 	char fileName[] = "./kernel_3.cl";
 	char kernelName[] = "kernel_3";
 
+	/* Get Platform and Device Info */
+	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	checkError(ret, "Couldn't get platform ids");
+	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
+	checkError(ret, "Couldn't get device ids");
+	ret = clGetDeviceInfo(device_id, CL_DEVICE_NAME, 0, NULL, &infoSize);
+	checkError(ret, "Couldn't get device info size");
+	info = (char*)malloc(infoSize);
+	ret = clGetDeviceInfo(device_id, CL_DEVICE_NAME, infoSize, info, NULL);
+	checkError(ret, "Couldn't get device info value");
+	printf("Running on %s\n\n", info);
+
 	/* Get user input */
 	printf("Select kernel (1-3)\n");
 	int kernelSelect = getInt();
 	printf("\nSelect array size\n");
 	int arraySize = getInt();
-	printf("\nSelect workgroup size (0 = auto,  max ..., )\n");
-	int workgroupSize = getInt();
+
 
 	/* Generate array */
 	printf("\nGenerating array, this could take some time...\n");
@@ -65,27 +78,6 @@ int main() {
 		strcpy(fileName, "./kernel_3.cl");
 		strcpy(kernelName, "kernel_3");
 	}
-	
-	/* Calculate workgroup size */
-	int lclSize;
-	if (kernelSelect == 3) {
-		lclSize = workgroupSize / 2;
-	}
-	else {
-		lclSize = workgroupSize;
-	}
-
-	/* Get Platform and Device Info */
-	ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-	checkError(ret, "Couldn't get platform ids");
-	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, &ret_num_devices);
-	checkError(ret, "Couldn't get device ids");
-	ret = clGetPlatformInfo(platform_id, CL_PLATFORM_NAME, 0, NULL, &infoSize);
-	checkError(ret, "Couldn't get platform info");
-	info = (char*)malloc(infoSize);
-	ret = clGetPlatformInfo(platform_id, CL_PLATFORM_NAME, infoSize, info, NULL);
-	checkError(ret, "Couldn't get platform attribute value");
-	printf("\nRunning on %s\n\n", info);
 
 	/* Create OpenCL Context */
 	context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
@@ -99,21 +91,16 @@ int main() {
 	dev_g_data = clCreateBuffer(context, CL_MEM_READ_WRITE,  sizeof(numbers) * arraySize, NULL, &ret);
 	checkError(ret, "Couldn't create global data on device");
 
-	/* Get current time before calculating the fractal */
-	LARGE_INTEGER freq, start;
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&start);
+	/* Get current time before write */
+	QueryPerformanceFrequency(&timeWriteFreq);
+	QueryPerformanceCounter(&timeWriteStart);
 
 	/* Copy arrays from host memory to Compute Devive */
 	ret = clEnqueueWriteBuffer(command_queue, dev_g_data, CL_TRUE, 0, sizeof(numbers) * arraySize, numbers, 0, NULL, NULL);
 	checkError(ret, "Couldn't write numbers on device");
 
-	/* Get current time after calculating the fractal */
-	LARGE_INTEGER end;
-	QueryPerformanceCounter(&end);
-
-	/* Print elapsed time */
-	printf("Elapsed time to copy to GPU:   %f msec\n", (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0);
+	/* Get current time after write */
+	QueryPerformanceCounter(&timeWriteEnd);
 
 	/* Create kernel program */
 	program = build_program(context, device_id, fileName);
@@ -123,38 +110,62 @@ int main() {
 	kernel = clCreateKernel(program, kernelName, &ret);
 	checkError(ret, "Couldn't create kernel");
 
+	/* Get max workgroup size */
+	size_t maxWorkgroupSize;
+	ret = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(size_t), &maxWorkgroupSize, NULL);
+	checkError(ret, "Couldn't get kernel info");
+
+	/* Get user input */
+	printf("\nSelect workgroup size (1-%d)\n", maxWorkgroupSize);
+	int workgroupSize = getInt();
+
 	/* Set OpenCL kernel arguments */
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&dev_g_data);
 	checkError(ret, "Couldn't set arg dev_g_data");
-	ret = clSetKernelArg(kernel, 1, lclSize * sizeof(int), NULL);
+	ret = clSetKernelArg(kernel, 1, workgroupSize * sizeof(int), NULL);
 	checkError(ret, "Couldn't set arg dev_l_data");
 
 	/* Get current time before calculating the fractal */
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&start);
+	QueryPerformanceFrequency(&timeCalcFreq);
+	QueryPerformanceCounter(&timeCalcStart);
 
 	/* Activate OpenCL kernel on the Compute Device */
 	
 	size_t globalSize[] = { arraySize };
-	size_t localSize[] = { lclSize };
+	size_t localSize[] = { workgroupSize };
 
-	// TODO: SPLIT INTO MORE KERNELS DEPENDING ON ARRAY SIZE
+	/* Split into kernels depending on array size*/
+	//DEBUG int i = 0;
+	while (globalSize[0] >= localSize[0] && localSize[0] > 1) {
+		//DEBUG printf("\nIteration:   %d\n", i);
+		//DEBUG printf("Global size: %d\n", globalSize[0]);
+		//DEBUG printf("Local size:  %d\n", localSize[0]);*/
 
-	clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, globalSize, localSize, 0, NULL, NULL);
-	checkError(ret, "Couldn't run kernel");
+		/* Execute kernel */
+		ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, globalSize, localSize, 0, NULL, NULL);
+		checkError(ret, "Couldn't run kernel");
+
+		/* Calculate new global size for next iteration */
+		if (strcmp(kernelName, "kernel_3") == 0) {
+			globalSize[0] = globalSize[0] / localSize[0] / 2;
+		} else {
+			globalSize[0] = globalSize[0] / localSize[0];
+		}
+		if (globalSize[0] < localSize[0]) {
+			localSize[0] = globalSize[0];
+		}		
+		//DEBUG i++;
+	}
 
 	/* Add blocking element */
 	clFinish(command_queue);
 
 	/* Get current time after calculating the fractal */
-	QueryPerformanceCounter(&end);
-
-	/* Print elapsed time */
-	printf("Elapsed time for kernel calls: %f msec\n", (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0);
+	QueryPerformanceCounter(&timeCalcEnd);
 
 	/* Get current time before calculating the fractal */
-	QueryPerformanceFrequency(&freq);
-	QueryPerformanceCounter(&start);
+	QueryPerformanceFrequency(&timeReadFreq);
+	QueryPerformanceCounter(&timeReadStart);
 
 	/* Transfer result array C back to host */
 	ret = clEnqueueReadBuffer(command_queue, dev_g_data, CL_TRUE, 0, sizeof(ret_number), ret_number, 0, NULL, NULL);
@@ -164,10 +175,7 @@ int main() {
 	clFinish(command_queue);
 
 	/* Get current time after calculating the fractal */
-	QueryPerformanceCounter(&end);
-
-	/* Print elapsed time */
-	printf("Elapsed time to read from GPU: %f msec\n\n", (double)(end.QuadPart - start.QuadPart) / freq.QuadPart * 1000.0);
+	QueryPerformanceCounter(&timeReadEnd);
 
 	/* Finalization */
 	ret = clFlush(command_queue);
@@ -179,6 +187,9 @@ int main() {
 	ret = clReleaseContext(context);
 
 	/* Print result */
+	printf("\nElapsed time to copy to GPU:   %f msec\n", (double)(timeWriteEnd.QuadPart - timeWriteStart.QuadPart) / timeWriteFreq.QuadPart * 1000.0);
+	printf("Elapsed time for kernel calls: %f msec\n", (double)(timeCalcEnd.QuadPart - timeCalcStart.QuadPart) / timeCalcFreq.QuadPart * 1000.0);
+	printf("Elapsed time to read from GPU: %f msec\n\n", (double)(timeReadEnd.QuadPart - timeReadStart.QuadPart) / timeReadFreq.QuadPart * 1000.0);
 	printf("Kernel used:  %s\n", fileName);
 	printf("Array length: %d\n\n", arraySize);
 	printf("Result:       %d\n\n", ret_number[0]);
