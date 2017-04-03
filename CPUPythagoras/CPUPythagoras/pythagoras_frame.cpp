641d9d7e48129a24ec391a6c7596cf6585b3e663
Jaeqx,Lars L.J.J <l.jaeqx@student.fontys.nl>
2017-04-03 18:28:14 +0200

    Cleanup

--- CPUPythagoras/CPUPythagoras/pythagoras_frame.cpp
+++ CPUPythagoras/CPUPythagoras/pythagoras_frame.cpp
@@ -3,7 +3,7 @@
 #include <GL\freeglut.h>
 
 #define WINDOW_SIZE 700
-#define MAXITERATIONS 40
+#define MAXITERATIONS 20
 #define ANGLE 45
 
 void pytharos_fractal(float topLeftx, float topLefty, float topRightx, float topRighty, float n, int iterations)
@@ -25,15 +25,15 @@ void pytharos_fractal(float topLeftx, float topLefty, float topRightx, float top
 	float Q4y = (Q1y - Q2y) + Q3y;
 
 	//display filled square
-	glBegin(GL_POLYGON);
+	/*glBegin(GL_POLYGON);
 	glColor3f(n*n/1.5, n*n / 2, n*n / 2.5);
 	glVertex3f(Q1x, Q1y, 0.0);
 	glVertex3f(Q2x, Q2y, 0.0);
 	glVertex3f(Q3x, Q3y, 0.0);
 	glVertex3f(Q4x, Q4y, 0.0);
-	glEnd();
+	glEnd();*/
 
-	/*//display square outline
+	//display square outline
 	glColor3f(n, n, n);
 	glBegin(GL_LINES);
 	glVertex3f(Q1x, Q1y, 0.0);
@@ -45,16 +45,9 @@ void pytharos_fractal(float topLeftx, float topLefty, float topRightx, float top
 	glVertex3f(Q4x, Q4y, 0.0);
 	glVertex3f(Q1x, Q1y, 0.0);
 	glEnd();
-	//*/
 
 	//length of a side of the square
 	float dist = sqrt(((Q1x - Q2x)*(Q1x - Q2x)) + ((Q1y - Q2y)*(Q1y - Q2y)));
-
-	/*
-	float a = cos(ANGLE) * dist;
-	float ACx = cos(ANGLE) * a;
-	float ACy = sin(ANGLE) * a;
-	*/
 
 	//calculate begining of next square
 	float ACx = (Q3x - Q1x) / 2;	//half of the diagonal
@@ -62,13 +55,9 @@ void pytharos_fractal(float topLeftx, float topLefty, float topRightx, float top
 	float newx = ACx + Q4x;		// point of iscoceles right triangle
 	float newy = ACy + Q4y;		// off of the bottom of the square
 
-
-	
-
-	// 1/700 = x/20
-	//20/700 = 1 px?
-	if (iterations < MAXITERATIONS) { //if the square is larger than 1 pixel
-		pytharos_fractal(Q4x, Q4y, newx, newy, n + 0.03, iterations + 2); //left recursion
-		pytharos_fractal(newx, newy, Q3x, Q3y, n + 0.08, iterations + 2); //right recursion
+	//calculate next iteration 
+	if (iterations < MAXITERATIONS) {
+		pytharos_fractal(Q4x, Q4y, newx, newy, n + 0.03, iterations + 1); //left recursion
+		pytharos_fractal(newx, newy, Q3x, Q3y, n + 0.08, iterations + 1); //right recursion
 	}
 }
--- CPUPythagoras/CPUPythagoras/pythagoras_frame.h
+++ CPUPythagoras/CPUPythagoras/pythagoras_frame.h
@@ -1,5 +1,1 @@
-typedef struct {
-	unsigned char blue, green, red;
-} mandelbrot_color;
-
 void pytharos_fractal(float topLeftx, float topLefty, float topRightx, float topRighty, float n, int iterations);
